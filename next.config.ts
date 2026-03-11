f6596e945ab75bec6f99cda911b4570c8047cf99
trendifii <trendifii6@gmail.com>
2026-03-11 16:23:02 +0000

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        hostname: "ik.imagekit.io",
+        protocol: "https"
+      },
+      {
+        hostname: "images.unsplash.com",
+        protocol: "https"
+      }
+    ]
+  }
 };
 
 export default nextConfig;
